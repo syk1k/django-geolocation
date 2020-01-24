d25d72452bd85f2d63edb1b964d5023b6cf22a7b
syk1k <dambemondo@gmail.com>
2020-01-24 06:25:22 -0900

    Test with point field ok

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PointModel
 
 # Register your models here.
+
+@admin.register(PointModel)
+class PointModelAdmin(admin.ModelAdmin):
+    pass
+    
+
--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from geolocation_fields.models import fields
 
 # Create your models here.
+
+class PointModel(models.Model):
+    point = fields.PointField(verbose_name='Point')
